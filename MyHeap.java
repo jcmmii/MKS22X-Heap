0f244e5fd6548d737c4f291ca530dcd538ce23fb
Justin Chen <jchen06@stuy.edu>
2019-04-27 01:18:23 -0400

    More progress on pushDown, currently broken

--- MyHeap.java
+++ MyHeap.java
@@ -1,36 +1,40 @@
 public class MyHeap {
   private static void pushDown(int[]data, int size, int index) {
     int temp;
-    if (index * 2 + 1 >= size) return;
-    if (data[2*index+1] > data[2*index+2]) {
-      if (data[index] < data[2*index+1]) {
-        temp = data[2*index+1];
+    if (index * 2 + 1 >= size) return; //base case when index is greater than size
+    if (index * 2 + 1 = size - 1) {
+      if (data[index] < data[index*2 + 1]) {
+        temp = data[index*2+1];
         data[2*index+1] = data[index];
         data[index] = temp;
       }
-    } else {
-      if (data[index] < data[2*index+2]) {
-        temp = data[2*index+2];
-        data[2*index+2] = data[index];
-        data[index] = temp;
-      }
-
     }
-
-
-
+    if (data[2*index+1] > data[2*index+2]) { //left child > right child
+      if (data[index] < data[2*index+1]) {   //if current index < left child, swaps
+        temp = data[2*index+1];
+        data[2*index+1] = data[index];
+        data[index] = temp;
+        pushDown(data,size,2*index+1);
+      }
+    } else { // left child < right child
+        if (data[index] < data[2*index+2]) {        //if current index < right child, swaps
+          temp = data[2*index+2];
+          data[2*index+2] = data[index];
+          data[index] = temp;
+          pushDown(data,size,2*index+2);
+        }
+      }
   }
-
-  private static void pushUp(int[]data,int index) {
-
-  }
-
-  public static void heapify(int[]) {
-
-  }
-
-  public static void heapsort(int[]) {
-
-  }
-
 }
+
+//  private static void pushUp(int[]data,int index) {
+
+  }
+
+//public static void heapify(int[]) {
+
+  }
+
+//  public static void heapsort(int[]) {
+
+  }
