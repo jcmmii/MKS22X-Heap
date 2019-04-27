f4d4a0dbcd2440aa3fb8de03254551a10da05428
Justin Chen <jchen06@stuy.edu>
2019-04-27 00:57:54 -0400

    Beginning pushDown method

--- MyHeap.java
+++ MyHeap.java
@@ -1,5 +1,23 @@
 public class MyHeap {
-  private static void pushDown(int[]data,int size,int index) {
+  private static void pushDown(int[]data, int size, int index) {
+    int temp;
+    if (index * 2 + 1 >= size) return;
+    if (data[2*index+1] > data[2*index+2]) {
+      if (data[index] < data[2*index+1]) {
+        temp = data[2*index+1];
+        data[2*index+1] = data[index];
+        data[index] = temp;
+      }
+    } else {
+      if (data[index] < data[2*index+2]) {
+        temp = data[2*index+2];
+        data[2*index+2] = data[index];
+        data[index] = temp;
+      }
+
+    }
+
+
 
   }
 
