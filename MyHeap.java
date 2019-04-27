bde413e886f58ddfc439b7aab6d865c526c9f3c8
Justin Chen <jchen06@stuy.edu>
2019-04-27 15:12:01 -0400

    more progress on pushDown

--- MyHeap.java
+++ MyHeap.java
@@ -2,32 +2,33 @@ public class MyHeap {
   private static void pushDown(int[]data, int size, int index) {
     int temp;
     if (index * 2 + 1 >= size) return; //base case when index is greater than size
-    if (index * 2 + 1 = size - 1) {
-      if (data[index] < data[index*2 + 1]) {
+    if (index * 2 + 1 = size - 1) { //when one child, at the end
+      if (data[index] < data[index*2 + 1]) { //if child is greater
         temp = data[index*2+1];
         data[2*index+1] = data[index];
         data[index] = temp;
       }
     }
-    if (data[2*index+1] > data[2*index+2]) { //left child > right child
-      if (data[index] < data[2*index+1]) {   //if current index < left child, swaps
-        temp = data[2*index+1];
-        data[2*index+1] = data[index];
-        data[index] = temp;
-        pushDown(data,size,2*index+1);
-      }
-    } else { // left child < right child
-        if (data[index] < data[2*index+2]) {        //if current index < right child, swaps
-          temp = data[2*index+2];
-          data[2*index+2] = data[index];
+    if (index * 2 + 1 != size - 1) {  //not at the end
+      if (data[2*index+1] > data[2*index+2]) { //left child > right child
+        if (data[index] < data[2*index+1]) {   //if current index < left child, swaps
+          temp = data[2*index+1];
+          data[2*index+1] = data[index];
           data[index] = temp;
-          pushDown(data,size,2*index+2);
+          pushDown(data,size,2*index+1);
+        }
+      } else if (data[2*index+1] < data[2*index+2]){ // left child < right child
+          if (data[index] < data[2*index+2]) {   //if current index < right child, swaps
+            temp = data[2*index+2];
+            data[2*index+2] = data[index];
+            data[index] = temp;
+            pushDown(data,size,2*index+2);
+          }
         }
       }
-  }
-}
+    }
 
-//  private static void pushUp(int[]data,int index) {
+  private static void pushUp(int[]data,int index) {
 
   }
 
